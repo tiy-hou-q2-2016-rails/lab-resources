58807f6b49bd65a77758bf2026208b27c0bbb457
Jesse Wolgamott <jesse@comalproductions.com>
2016-06-10 14:45:37 -0500

    Zoo.tdd

--- lab3/zoo.rb
+++ lab3/zoo.rb
@@ -1,8 +1,38 @@
 class Zoo
 
-  attr_accessor :city
+  attr_accessor :city, :animals
 
   def initialize(city_name)
     @city = city_name
+    @animals = []
+    @hungry_animals = []
+  end
+
+  def add_animal(animal)
+    @animals << animal
+    @hungry_animals << animal
+  end
+
+  def hungry?(animal)
+    @hungry_animals.include? animal
+  end
+
+  def feed(animal)
+    # remove from the hungry animals array
+    # [:zebra, :tortoise]
+    index_of_animal_in_array = @hungry_animals.index( animal)
+    @hungry_animals.slice!( index_of_animal_in_array  )
+  end
+
+  def full?(animal)
+    full_animals.include?(animal)
+  end
+
+  def full_animals
+    @animals - @hungry_animals
+  end
+
+  def advance_day
+    @hungry_animals = @animals
   end
 end
--- lab3/zoo_test.rb
+++ lab3/zoo_test.rb
@@ -3,9 +3,53 @@ require './zoo'
 
 class ZooTest < MiniTest::Test
 
-  def test_zoos_are_in_a_city
+  def setup
+    @zoo = Zoo.new "Houston, TX"
+  end
 
-    zoo = Zoo.new "Houston, TX"
-    assert_equal "Houston, TX", zoo.city
+
+  def test_zoos_are_in_a_city
+    assert_equal "Houston, TX", @zoo.city
+  end
+
+  def test_zoos_can_have_an_animal
+    @zoo.add_animal(:zebra)
+
+    assert_includes @zoo.animals, :zebra
+  end
+
+  def test_can_feed_animals
+    @zoo.add_animal(:zebra)
+
+    assert_equal true, @zoo.hungry?(:zebra)
+
+    @zoo.feed(:zebra)
+
+    assert_equal false, @zoo.hungry?(:zebra)
+  end
+
+  def test_can_see_the_fed_animals
+    @zoo.add_animal(:zebra)
+    @zoo.add_animal(:lions)
+    @zoo.add_animal(:snakes)
+    @zoo.feed(:zebra)
+    @zoo.feed(:lions)
+
+    assert_equal true, @zoo.hungry?(:snakes)
+    assert_equal true, @zoo.full?(:zebra)
+    assert_equal true, @zoo.full?(:lions)
+  end
+
+  def test_if_a_new_day_all_animals_are_hungry
+
+    @zoo.add_animal(:zebra)
+    @zoo.add_animal(:lions)
+    @zoo.feed(:zebra)
+    @zoo.feed(:lions)
+
+    @zoo.advance_day
+    assert_equal true, @zoo.hungry?(:zebra)
+    assert_equal true, @zoo.hungry?(:lions)
+
   end
 end
